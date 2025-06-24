1091bf168fdf64fa0702ccdac4b9c3bd83d17085
jaimehenao8126 <148729312+jaimehenao8126@users.noreply.github.com>
2025-06-24 12:09:05 -0500

    Improve descriptions for placeholder commands

--- cmd/diagnose.go
+++ cmd/diagnose.go
@@ -9,13 +9,10 @@ import (
 // diagnoseCmd represents the diagnose command
 var diagnoseCmd = &cobra.Command{
 	Use:   "diagnose",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Ejecuta chequeos de diagnóstico",
+	Long: `Realiza pruebas básicas en Pods, Services e Ingresses para
+detectar configuraciones erróneas o estados anómalos.
+Actualmente solo imprime un mensaje de prueba.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("diagnose called")
 	},
@@ -23,14 +20,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(diagnoseCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// diagnoseCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// diagnoseCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
